638bfb5fe9f81ed9a139321348edab66e5c41247
stfcrux <jinweiloh1998@gmail.com>
2018-09-06 00:27:47 +1000

    update

--- Assets/PointLight.cs
+++ Assets/PointLight.cs
@@ -1,43 +1,33 @@
-﻿
 using UnityEngine;
 using System.Collections;
 
 public class PointLight : MonoBehaviour
 {
+    // The color of the point light
+    public Color color = Color.white;
 
-    public Color color;
-    public int multiplier;
+    // the speed of the point light's rotation
+    public int rotationSpeed;
 
-    // Rotation will be used as a speed element for the rotation (aka sunsetting and rising)
+    // the position of the rotation
     private float rotation;
-
-    // Multiplier value to modify rotation speed!
 
     public Vector3 GetWorldPosition()
     {
         return this.transform.position;
     }
 
-    //	Initialize
-    void Start()
-    {
-        // Set initial rotation radius
-        CreateMountainPlane planeObject = GameObject.Find("Mountain").GetComponent<CreateMountainPlane>();
-        gameObject.transform.position = new Vector3(0, planeObject.sideLength / 2, -planeObject.sideLength / 2);
-    }
-
-
-    // Perform Rotation and keep the sun facing the origin
+    // Perform Rotation of point light and making sure that the point light is facing the origin
     public void Update()
     {
-        // Time.deltaTime gets the time between frames
-        rotation = Time.deltaTime * multiplier; //The greater the multiplier the faster the speed.
+        // setting the position of the point light
+        rotation = Time.deltaTime * rotationSpeed;
 
-        // Vector3.right is the same as new Vector3(1,0,0) (x axis is constant, sun rotates around z and y).
+        // Vector3.right is the vector facing the world right
+        // x axis is always constant and the sun rotates around y and z
         transform.RotateAround(Vector3.zero, Vector3.right, rotation);
 
-        // Transform at every frame to ensure light is looking at (0,0,0)
+        // Transform at every frame to ensure light is looking at origin
         transform.LookAt(Vector3.zero);
     }
 }
-
